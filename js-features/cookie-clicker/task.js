e39e7f5fa3bafd4772e59bb25b302f9cfbd41471
Orekhova <irinamarkina803@gmail.com>
2024-10-01 09:59:24 +0400

    Countdown

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,15 +1,1 @@
 "use strict";
-
-const decreaseNumber = function () {
-  const newText = document.getElementById("clicker__counter");
-  newText.textContent--;
-
-  if (newText.textContent < 0) {
-    alert("Вы победили в конкурсе!");
-    clearInterval(timerId);
-    newText.textContent = 0;
-  }
-};
-let timerId = setInterval(decreaseNumber, 400);
-
-console.log(timerId);
