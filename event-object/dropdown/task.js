b5fafe6a40730bd5465c354f998edbf705b0acf4
Orekhova <irinamarkina803@gmail.com>
2024-10-08 09:18:57 +0400

    Task1 changed

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -5,15 +5,11 @@ const list = document.querySelector(".dropdown__list");
 const linksList = Array.from(document.querySelectorAll(".dropdown__link"));
 
 button.addEventListener("click", () => {
-  if (list.classList.contains("dropdown__list_active") == true) {
-    list.classList.remove("dropdown__list_active");
-  } else {
-    list.classList.add("dropdown__list_active");
-  }
+  list.classList.toggle("dropdown__list_active");
 });
-
 linksList.forEach((item, index) => {
-  item.onclick = () => {
+  item.onclick = (e) => {
+    e.preventDefault();
     button.textContent = linksList[index].textContent;
   };
 });
