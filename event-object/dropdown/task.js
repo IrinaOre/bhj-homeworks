887c525b3e357e83d135145b2071335b4f58c21f
Orekhova <irinamarkina803@gmail.com>
2024-10-08 11:48:01 +0400

    Task1, Task2

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -4,6 +4,7 @@ const button = document.querySelector(".dropdown__value");
 const list = document.querySelector(".dropdown__list");
 const linksList = Array.from(document.querySelectorAll(".dropdown__link"));
 
+// 1st variant:
 button.addEventListener("click", () => {
   list.classList.toggle("dropdown__list_active");
 });
@@ -11,5 +12,6 @@ linksList.forEach((item, index) => {
   item.onclick = (e) => {
     e.preventDefault();
     button.textContent = linksList[index].textContent;
+    list.classList.remove("dropdown__list_active");
   };
 });
